06623d49aa504b99856d1fa53dd9ecdfbcbe785d
Atul Bipin <atulbipin@gmail.com>
2019-02-24 14:19:12 -0800

    Change start-service.sh script

--- start-service.sh
+++ start-service.sh
@@ -3,8 +3,14 @@
 eval `docker-machine env ezplanner-sandbox`
 
 docker container stop ezplanner-planner-service
-docker container rm ezplanner-planner-service
+docker rm -f ezplanner-planner-service
+
+docker rmi ezplanner-planner-service
+
+docker image prune
+
+docker volume prune
 
 docker build -t ezplanner-planner-service .
 
-docker run --name ezplanner-planner-service -l=apiRoute='/plan' -p 8000:5000 -d ezplanner-planner-service
+docker run --name ezplanner-planner-service -l=apiRoute='/plan' -p 5000:5000 -d ezplanner-planner-service
