6fab94b4fa1b8d06151d6f33c6dd4b47fbe84d78
Atul Bipin <atulbipin@gmail.com>
2019-03-23 12:16:14 -0700

    Fix planner logic

--- controllers/plan.js
+++ controllers/plan.js
@@ -1,5 +1,5 @@
-const prereqMap = require('../data/prereq.json');
-const postreqMap = require('../data/postreq.json');
+const prereqMap = require("../data/prereq.json");
+const postreqMap = require("../data/postreq.json");
 
 let defaultPlanner;
 
@@ -15,10 +15,13 @@ class Planner {
 
     plan(finishedCourses) {
         // TODO: If we need to improve performance further, reduce function calls and use basic for loops
-        const finishedCourseMap = finishedCourses.reduce((acc, course) => ({
-            ...acc,
-            [course]: true
-        }), {});
+        const finishedCourseMap = finishedCourses.reduce(
+            (acc, course) => ({
+                ...acc,
+                [course]: true
+            }),
+            {}
+        );
 
         const check = items => {
             if (!items) {
@@ -29,33 +32,49 @@ class Planner {
             let required = 0;
 
             for (const item of items) {
-                if (Array.isArray(item)) {
-                    if (!check(item)) {
-                        return false;
-                    }
-                } else if (!isNaN(item)) {
+                const checkIfCourseIsTaken = course => Boolean(finishedCourseMap[course]);
+
+                if (!isNaN(item)) {
                     required = Number(item);
                 } else {
-                    if (!finishedCourseMap[item] && required == 0) {
+                    let checkIfItemIsSatisfied;
+
+                    if (Array.isArray(item)) {
+                        checkIfItemIsSatisfied = check;
+                    } else {
+                        checkIfItemIsSatisfied = checkIfCourseIsTaken;
+                    }
+
+                    const itemIsSatisfied = checkIfItemIsSatisfied(item);
+
+                    if (!itemIsSatisfied && required === 0) {
                         return false;
-                    } else if (finishedCourseMap[item]) {
+                    } else if (itemIsSatisfied) {
                         taken += 1;
+
+                        if (required !== 0 && taken >= required) {
+                            return true;
+                        }
                     }
                 }
             }
 
             return required === 0 || taken >= required;
-        }
+        };
 
         const unique = array => [...new Set(array)];
 
         const possibleCourses = unique(
             finishedCourses
-                .flatMap(finishedCourse => this.postreqMap[finishedCourse] || [])
+                .flatMap(
+                    finishedCourse => this.postreqMap[finishedCourse] || []
+                )
                 .filter(possibleCourse => !finishedCourseMap[possibleCourse])
         );
 
-        return possibleCourses.filter(possibleCourse => check(this.prereqMap[possibleCourse]));
+        return possibleCourses.filter(possibleCourse =>
+            check(this.prereqMap[possibleCourse])
+        );
     }
 }
 
@@ -68,5 +87,6 @@ class PlanController {
 }
 
 module.exports = {
-    PlanController
+    PlanController,
+    Planner
 };
